e0ebd24211c8fc98faabbb6b1ae9db721c05f962
Álvaro Herero <alvaro.herrero11x@gmail.com>
2025-01-14 16:39:36 +0100

    Movimientos Repository Implemented

---
+++ VivesBankApi/Rest/Movimientos/Repositories/IMovimientosRepository.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using VivesBankApi.Rest.Movimientos.Models;
+
+namespace VivesBankApi.Rest.Movimientos.Repositories;
+
+public interface IMovimientosRepository
+{
+    Task<List<Movimiento>> GetAllMovimientosAsync();
+    Task<Movimiento> GetMovimientoByIdAsync(ObjectId id);
+    Task<Movimiento> AddMovimientoAsync(Movimiento movimiento);
+    Task<Movimiento> UpdateMovimientoAsync(ObjectId id, Movimiento movimiento);
+    Task<Movimiento> DeleteMovimientoAsync(ObjectId id);
+}
