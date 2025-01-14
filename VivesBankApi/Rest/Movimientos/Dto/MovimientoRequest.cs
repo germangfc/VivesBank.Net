814f5531157f764627788a791d4dde291d7bcedd
germanGFC <147338370+germangfc@users.noreply.github.com>
2025-01-14 16:47:05 +0100

    movimientos dtos

---
+++ VivesBankApi/Rest/Movimientos/Dto/MovimientoRequest.cs
@@ -0,0 +1,13 @@
+﻿using VivesBankApi.Rest.Movimientos.Models;
+using VivesBankApi.utils.GuuidGenerator;
+
+namespace VivesBankApi.Rest.Movimientos.Dto;
+
+public class MovimientoRequest
+{
+    public string guid { get; set; } = GuuidGenerator.GenerateHash();
+    public string clienteGuid { get; set; }
+    public Domiciliacion domiciliacion { get; set; }
+    public IngresoDeNomina ingresoDeNomina { get; set; }
+    public PagoConTarjeta pagoConTarjeta { get; set; }
+}
