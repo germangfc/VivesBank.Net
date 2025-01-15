0761c5f243ae9a1da00d3dd844c8f1772a76f7e4
Diego NL <diegonoviluce28@gmail.com>
2025-01-15 20:06:19 +0100

    Cambio repositorio

--- VivesBankApi/Rest/Users/Repository/IUserRepository.cs
+++ VivesBankApi/Rest/Users/Repository/IUserRepository.cs
@@ -1,8 +1,9 @@
+using ApiFunkosCS.Utils.GenericRepository;
 using VivesBankApi.Rest.Users.Models;
 
 namespace VivesBankApi.Rest.Users.Repository;
 
-public interface IUserRepository
+public interface IUserRepository : IGenericRepository<User>
 {
     Task<User?> GetByUsernameAsync(String username);
 }
