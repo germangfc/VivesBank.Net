15a95e88cba88bbca1520c1ce030abf62f63570a
germanGFC <147338370+germangfc@users.noreply.github.com>
2025-01-17 00:04:48 +0100

    domiciliacion service tests finished

--- Tests/Rest/Movimientos/Services/Domiciliaciones/DomiciliacionServiceTest.cs
+++ Tests/Rest/Movimientos/Services/Domiciliaciones/DomiciliacionServiceTest.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 using NUnit.Framework.Legacy;
 using VivesBankApi.Rest.Movimientos.Config;
+using VivesBankApi.Rest.Movimientos.Exceptions;
 using VivesBankApi.Rest.Movimientos.Models;
 using VivesBankApi.Rest.Movimientos.Repositories.Domiciliaciones;
 using VivesBankApi.Rest.Movimientos.Services.Domiciliaciones;
@@ -16,17 +18,23 @@ namespace Tests.Rest.Movimientos.Services.Domiciliaciones;
 public class DomiciliacionServiceTest
 {
     private readonly IOptions<MongoDatabaseConfig> _mongoDatabaseSettings;
-    private Mock<DomiciliacionRepository> _repositoryMock;
+    private Mock<IDomiciliacionRepository> _repositoryMock;
     private Mock<ILogger<DomiciliacionService>> _loggerMock;
-    private readonly IOptions<ApiConfig> _apiConfig;
+    private IOptions<ApiConfig> _apiConfig;
     private DomiciliacionService _domiciliacionService;
     private List<Domiciliacion> _expectedDomiciliacionList;
 
     [SetUp]
     public void Setup()
     {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        _apiConfig = Options.Create(configuration.GetSection("ApiBasicConfig").Get<ApiConfig>());
         _loggerMock = new Mock<ILogger<DomiciliacionService>>();
-        _repositoryMock = new Mock<DomiciliacionRepository>(_mongoDatabaseSettings,_loggerMock);
+        _repositoryMock = new Mock<IDomiciliacionRepository>();
         _domiciliacionService = new DomiciliacionService(_repositoryMock.Object, _loggerMock.Object, _apiConfig);
         _expectedDomiciliacionList = new List<Domiciliacion>
         {
@@ -57,23 +65,148 @@ public class DomiciliacionServiceTest
         };
     }
     [Test]
-    public void FindAllDomiciliaciones()
+    public async Task FindAllDomiciliaciones()
     {
         // Arrange
         _repositoryMock.Setup(repo => repo.GetAllDomiciliacionesAsync())
             .ReturnsAsync(_expectedDomiciliacionList);
 
-        // Test
-        var list = _domiciliacionService.FindAllDomiciliacionesAsync();
+        // Act
+        var result = await _domiciliacionService.FindAllDomiciliacionesAsync();
 
-        // Comprobaciones
+        // Assert
         Assert.Multiple(() =>
         {
-            ClassicAssert.IsNotNull(list);
-            ClassicAssert.AreEqual(3, list.Result.Count);
+            ClassicAssert.IsNotNull(result);
+            ClassicAssert.AreEqual(2, result.Count);
+            ClassicAssert.AreEqual(_expectedDomiciliacionList[0].Guid, result[0].Guid);
+            ClassicAssert.AreEqual(_expectedDomiciliacionList[1].Guid, result[1].Guid);
+            ClassicAssert.AreEqual(_expectedDomiciliacionList[0].ClienteGuid, result[0].ClienteGuid);
+            ClassicAssert.AreEqual(_expectedDomiciliacionList[1].ClienteGuid, result[1].ClienteGuid);
         });
 
-        // Verificamos que se ha llamado al método
         _repositoryMock.Verify(repo => repo.GetAllDomiciliacionesAsync(), Times.Once);
     }
+    
+    [Test]
+    public async Task FindDomiciliacionByIdAsyncOk()
+    {
+        
+        // Arrange
+        const string id = "1";
+        var domiciliacion = _expectedDomiciliacionList.First();
+        _repositoryMock.Setup(repo => repo.GetDomiciliacionByIdAsync(id))
+            .ReturnsAsync(domiciliacion);
+        
+        // Act
+        var result = await _domiciliacionService.FindDomiciliacionByIdAsync(id);
+        
+        // Assert
+        ClassicAssert.IsNotNull(result);
+        ClassicAssert.AreEqual(domiciliacion.Guid, result.Guid);
+        _repositoryMock.Verify(repo => repo.GetDomiciliacionByIdAsync(id), Times.Once);
+    }
+    
+    [Test]
+    public void FindDomiciliacionByIdAsyncIdNotFound()
+    {
+        // Arrange
+        const string id = "xxx";
+        _repositoryMock.Setup(repo => repo.GetDomiciliacionByIdAsync(id))!.ReturnsAsync((Domiciliacion)null);
+
+        // Act & Assert
+        var ex = Assert.ThrowsAsync<DomiciliacionNotFoundException>(async () => 
+            await _domiciliacionService.FindDomiciliacionByIdAsync(id));
+        
+        // Assert
+        ClassicAssert.AreEqual(ex.Message, $"Domiciliacion not found with ID {id}");
+        
+        _repositoryMock.Verify(repo => repo.GetDomiciliacionByIdAsync(id), Times.Once);
+    }
+    
+    [Test]
+    public async Task AddDomiciliacionAsyncOk()
+    {
+        // Arrange
+        var newDomiciliacion = _expectedDomiciliacionList.First();
+        _repositoryMock.Setup(repo => repo.AddDomiciliacionAsync(newDomiciliacion))
+            .ReturnsAsync(newDomiciliacion);
+        
+        // Act
+        var result = await _domiciliacionService.AddDomiciliacionAsync(newDomiciliacion);
+        
+        // Assert
+        ClassicAssert.IsTrue(result.Contains(_apiConfig.Value.BaseEndpoint));
+        
+        _repositoryMock.Verify(repo => repo.AddDomiciliacionAsync(newDomiciliacion), Times.Once);
+    }
+
+    [Test]
+    public async Task UpdateDomiciliacionAsyncOk()
+    {
+        // Arrange
+        const string id = "1";
+        var updatedDomiciliacion = _expectedDomiciliacionList.First();
+        _repositoryMock.Setup(repo => repo.UpdateDomiciliacionAsync(id, updatedDomiciliacion)).
+            ReturnsAsync(updatedDomiciliacion);
+        
+        // Act
+        var result = await _domiciliacionService.UpdateDomiciliacionAsync(id, updatedDomiciliacion);
+        
+        // Assert
+        ClassicAssert.IsTrue(result.Contains(_apiConfig.Value.BaseEndpoint));
+        _repositoryMock.Verify(repo => repo.UpdateDomiciliacionAsync(id, updatedDomiciliacion), Times.Once);
+    }
+
+    [Test]
+    public void UpdateDomiciliacionAsyncIdNotFound()
+    {
+        // Arrange
+        const string id = "xxx";
+        var updatedDomiciliacion = _expectedDomiciliacionList.First();
+        _repositoryMock.Setup(repo => repo.UpdateDomiciliacionAsync(id, updatedDomiciliacion))
+            .ReturnsAsync((Domiciliacion)null);
+        
+        // Act & Assert
+        var ex = Assert.ThrowsAsync<DomiciliacionNotFoundException>(async () => 
+            await _domiciliacionService.UpdateDomiciliacionAsync(id, updatedDomiciliacion));
+        
+        // Assert
+        ClassicAssert.AreEqual(ex.Message, $"Domiciliacion not found with ID {id}");
+        
+        _repositoryMock.Verify(repo => repo.UpdateDomiciliacionAsync(id, updatedDomiciliacion), Times.Once);
+    }
+    
+    [Test]
+    public async Task DeleteDomiciliacionAsyncOk()
+    {
+        // Arrange
+        const string id = "1";
+        var domiciliacion = _expectedDomiciliacionList.First();
+        _repositoryMock.Setup(repo => repo.DeleteDomiciliacionAsync(id)).ReturnsAsync(domiciliacion);
+        
+        // Act
+        var result = await _domiciliacionService.DeleteDomiciliacionAsync(id);
+        
+        // Assert
+        ClassicAssert.IsNotNull(result);
+        _repositoryMock.Verify(repo => repo.DeleteDomiciliacionAsync(id), Times.Once);
+    }
+
+    [Test]
+    public void DeleteDomiciliacionAsyncIdNotFound()
+    {
+        // Arrange
+        const string id = "invalidId";
+        _repositoryMock.Setup(repo => repo.DeleteDomiciliacionAsync(id)).ReturnsAsync((Domiciliacion)null);
+        
+        // Act & Assert
+        var ex = Assert.ThrowsAsync<DomiciliacionNotFoundException>(async () => await _domiciliacionService.DeleteDomiciliacionAsync(id));
+        
+        // Assert
+        ClassicAssert.AreEqual(ex.Message, $"Domiciliacion not found with ID {id}");
+        
+        _repositoryMock.Verify(repo => repo.DeleteDomiciliacionAsync(id), Times.Once);
+    }
+
 }
