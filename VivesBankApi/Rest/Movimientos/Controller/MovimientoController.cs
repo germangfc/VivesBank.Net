994fecc7c5e615aa6dc665fbc1b7ecee5bb31fe7
germanGFC <147338370+germangfc@users.noreply.github.com>
2025-01-16 16:56:16 +0100

    movimientoController corrections

--- VivesBankApi/Rest/Movimientos/Controller/MovimientoController.cs
+++ VivesBankApi/Rest/Movimientos/Controller/MovimientoController.cs
@@ -10,77 +10,66 @@ namespace VivesBankApi.Rest.Movimientos.Controller;
 
 [ApiController]
 [Route("api/[controller]")]
-public class MovimientoController : ControllerBase
+public class MovimientoController(
+    IMovimientoService movimientoService,
+    IUserRepository userService,
+    ILogger<MovimientoController> logger,
+    IHttpContextAccessor httpContextAccessor)
+    : ControllerBase
 {
-    private readonly IMovimientoService _movimientoService;
-    private readonly IUserRepository _userService;
-    private readonly ILogger<MovimientoController> _logger;
-    private readonly IHttpContextAccessor _httpContextAccessor;
-    
-    public MovimientoController(IMovimientoService movimientoService, 
-        IUserRepository userService, 
-        ILogger<MovimientoController> logger,
-        IHttpContextAccessor httpContextAccessor)
-    {
-        _movimientoService = movimientoService;
-        _userService = userService;
-        _httpContextAccessor = httpContextAccessor;
-        _logger = logger;
-    }
-    
     [Authorize]
-    [HttpPost]
+    [HttpPost("/domiciliacion/")]
     public async Task<ActionResult<Domiciliacion>> CreateDomiciliacion([FromBody] Domiciliacion domiciliacion)
     {
-        _logger.LogInformation("Creating new domiciliacion");
-        var user = _httpContextAccessor.HttpContext!.User;
+        logger.LogInformation("Creating new domiciliacion");
+        var user = httpContextAccessor.HttpContext!.User;
         var appUser = await ConvertClaimsPrincipalToUser(user);
-        return await _movimientoService.AddDomiciliacionAsync(appUser, domiciliacion);
+        return await movimientoService.AddDomiciliacionAsync(appUser, domiciliacion);
     }
     
     [Authorize]
-    [HttpPost]
+    [HttpPost("/transferencia/")]
     public async Task<ActionResult<Movimiento>> AddTransferencia([FromBody] Transferencia transferencia)
     {
-        _logger.LogInformation("Creating new transferencia");
-        var user = _httpContextAccessor.HttpContext!.User;
+        logger.LogInformation("Creating new transferencia");
+        var user = httpContextAccessor.HttpContext!.User;
         var appUser = await ConvertClaimsPrincipalToUser(user);
-        return await _movimientoService.AddTransferenciaAsync(appUser, transferencia);
+        return await movimientoService.AddTransferenciaAsync(appUser, transferencia);
     }
     
     [Authorize]
-    [HttpPost]
+    [HttpPost("/ingresonomina/")]
     public async Task<ActionResult<Movimiento>> AddIngresoDeNomina([FromBody] IngresoDeNomina ingresoDeNomina)
     {
-        _logger.LogInformation("Creating new ingreso de nomina");
-        var user = _httpContextAccessor.HttpContext!.User;
+        logger.LogInformation("Creating new ingreso de nomina");
+        var user = httpContextAccessor.HttpContext!.User;
         var appUser = await ConvertClaimsPrincipalToUser(user);
-        return await _movimientoService.AddIngresoDeNominaAsync(appUser, ingresoDeNomina);
+        return await movimientoService.AddIngresoDeNominaAsync(appUser, ingresoDeNomina);
     }
     [Authorize]
-    [HttpPost]
+    [HttpPost("/pagotarjeta/")]
     public async Task<ActionResult<Movimiento>> AddPagoConTarjeta([FromBody] PagoConTarjeta pagoConTarjeta)
     {
-        _logger.LogInformation("Creating new pago con tarjeta");
-        var user = _httpContextAccessor.HttpContext!.User;
+        logger.LogInformation("Creating new pago con tarjeta");
+        var user = httpContextAccessor.HttpContext!.User;
         var appUser = await ConvertClaimsPrincipalToUser(user);
-        return await _movimientoService.AddPagoConTarjetaAsync(appUser, pagoConTarjeta);
+        return await movimientoService.AddPagoConTarjetaAsync(appUser, pagoConTarjeta);
     }
     
     [Authorize]
-    [HttpPost("{transfGuid}")]
+    [HttpDelete("/transferencia/{transfGuid}")]
     public async Task<ActionResult<Movimiento>> RevocarTransferencia(string transfGuid)
     {
-        _logger.LogInformation("Revoking transferencia");
-        var user = _httpContextAccessor.HttpContext!.User;
+        logger.LogInformation("Revoking transferencia");
+        var user = httpContextAccessor.HttpContext!.User;
         var appUser = await ConvertClaimsPrincipalToUser(user);
-        return await _movimientoService.RevocarTransferencia(appUser, transfGuid);
+        return await movimientoService.RevocarTransferencia(appUser, transfGuid);
     }
     
     
     private async Task<User> ConvertClaimsPrincipalToUser(ClaimsPrincipal user)
     {
         var id = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        return await _userService.GetByIdAsync(id);
+        return await userService.GetByIdAsync(id);
     }
 }
