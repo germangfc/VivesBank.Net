71680b179c3cbc929e330556dfe557fb92ef13e9
Samuel Cortés Sánchez <samuelcortessanchez@gmail.com>
2025-01-17 19:59:18 +0100

    Test de los generadores de tarjeta implementados

---
+++ Tests/Rest/Product/CreditCard/Generators/ExpirationDateGeneratorTest.cs
@@ -0,0 +1,20 @@
+﻿using VivesBankApi.Rest.Product.CreditCard.Generators;
+
+namespace Tests.Rest.Product.CreditCard.Generators;
+
+[TestFixture]
+[TestOf(typeof(ExpirationDateGenerator))]
+public class ExpirationDateGeneratorTest
+{
+    private ExpirationDateGenerator _expirationDateGenerator;
+
+    [Test]
+    public void GenerateRandomDate()
+    {
+        _expirationDateGenerator = new ExpirationDateGenerator();
+        var date = _expirationDateGenerator.GenerateRandomDate();
+
+        Assert.That(date.Year, Is.GreaterThan(DateTime.Today.Year));
+        Assert.That(date.Year, Is.LessThan(DateTime.Today.Year + 6));
+    }
+}
