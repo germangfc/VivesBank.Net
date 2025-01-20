043a1f7b545216e90a88bd79ec7b01be606767f9
Samuel Cortés Sánchez <samuelcortessanchez@gmail.com>
2025-01-20 12:29:59 +0100

    Validador para la creacion del producto

---
+++ Tests/Rest/Product/Base/Validator/ProductValidatorTest.cs
@@ -0,0 +1,10 @@
+﻿using VivesBankApi.Rest.Product.Base.Validators;
+
+namespace Tests.Rest.Product.Base.Validator;
+
+[TestFixture]
+[TestOf(typeof(ProductValidator))]
+public class ProductValidatorTest
+{
+    
+}
---
+++ VivesBankApi/Rest/Product/Base/Validator/ProductValidator.cs
@@ -0,0 +1,22 @@
+﻿using VivesBankApi.Rest.Product.Base.Dto;
+
+namespace VivesBankApi.Rest.Product.Base.Validators;
+
+public class ProductValidator
+{
+    public static bool isValidProduct(ProductCreateRequest productRequest)
+        {
+            if (string.IsNullOrWhiteSpace(productRequest.Name))
+            {
+                return false;
+            }
+    
+            var validTypes = new[] { "BANKACCOUNT", "CREDITCARD" };
+            if (!validTypes.Contains(productRequest.Type))
+            {
+                return false;
+            }
+    
+            return true;
+        }
+}
