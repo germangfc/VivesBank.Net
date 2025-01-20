c8c4439744d7fe47d3c6f1065943b151a5073125
Samuel Cortés Sánchez <samuelcortessanchez@gmail.com>
2025-01-20 13:29:45 +0100

    Arreglados los test de productos

--- Tests/Rest/Product/Base/Validator/ProductValidatorTest.cs
+++ Tests/Rest/Product/Base/Validator/ProductValidatorTest.cs
@@ -1,4 +1,5 @@
-﻿using VivesBankApi.Rest.Product.Base.Validators;
+﻿using VivesBankApi.Rest.Product.Base.Dto;
+using VivesBankApi.Rest.Product.Base.Validators;
 
 namespace Tests.Rest.Product.Base.Validator;
 
@@ -6,5 +7,31 @@ namespace Tests.Rest.Product.Base.Validator;
 [TestOf(typeof(ProductValidator))]
 public class ProductValidatorTest
 {
+    private ProductValidator _productValidator;
+
+    [Test]
+    public void isValidProduct()
+    {
+        var product = new ProductCreateRequest { Name = "Test Product", Type = "BankAccount" };
+        var isValid = ProductValidator.isValidProduct(product);
+        Assert.That(isValid, Is.True);
+    }
+
+    [Test]
+    public void invalidProductName()
+    {
+        var product = new ProductCreateRequest { Type = "BankAccount" };
+        var isValid = ProductValidator.isValidProduct(product);
+        Assert.That(isValid, Is.False);
+    }
+
+    [Test]
+    public void invalidProductType()
+    {
+        var product = new ProductCreateRequest { Name = "Test Product" };
+        var isValid = ProductValidator.isValidProduct(product);
+        Assert.That(isValid, Is.False);
+    }
     
+
 }
--- VivesBankApi/Rest/Product/Base/Validator/ProductValidator.cs
+++ VivesBankApi/Rest/Product/Base/Validator/ProductValidator.cs
@@ -5,18 +5,19 @@ namespace VivesBankApi.Rest.Product.Base.Validators;
 public class ProductValidator
 {
     public static bool isValidProduct(ProductCreateRequest productRequest)
+    {
+        if (string.IsNullOrWhiteSpace(productRequest.Name))
         {
-            if (string.IsNullOrWhiteSpace(productRequest.Name))
-            {
-                return false;
-            }
-    
-            var validTypes = new[] { "BANKACCOUNT", "CREDITCARD" };
-            if (!validTypes.Contains(productRequest.Type))
-            {
-                return false;
-            }
-    
-            return true;
+            return false;
         }
+
+        var validTypes = new[] { "BankAccount", "CreditCard" };
+
+        if (!validTypes.Any(validType => string.Equals(validType, productRequest.Type, StringComparison.OrdinalIgnoreCase)))
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
