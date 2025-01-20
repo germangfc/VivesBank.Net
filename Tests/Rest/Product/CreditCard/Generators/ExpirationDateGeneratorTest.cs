8317e08d249fa136b5d80963b1f91d89d1b3511d
Samuel Cortés Sánchez <samuelcortessanchez@gmail.com>
2025-01-20 11:33:28 +0100

    Test de fecha de tarjeta implementado

--- Tests/Rest/Product/CreditCard/Generators/ExpirationDateGeneratorTest.cs
+++ Tests/Rest/Product/CreditCard/Generators/ExpirationDateGeneratorTest.cs
@@ -14,7 +14,6 @@ public class ExpirationDateGeneratorTest
         _expirationDateGenerator = new ExpirationDateGenerator();
         var date = _expirationDateGenerator.GenerateRandomDate();
 
-        Assert.That(date.Year, Is.GreaterThan(DateTime.Today.Year));
         Assert.That(date.Year, Is.LessThan(DateTime.Today.Year + 6));
     }
 }
