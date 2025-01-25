1c4393d0ee1d5a52cc6782c4b1a2a5deeb6cb8db
germanGFC <147338370+germangfc@users.noreply.github.com>
2025-01-25 18:45:15 +0100

    movimientos exceptions changes

---
+++ VivesBankApi/Rest/Movimientos/Exceptions/NotRevocableMovimientoException.cs
@@ -0,0 +1,4 @@
+﻿namespace VivesBankApi.Rest.Movimientos.Exceptions;
+
+public class MovimientoNotRevocableException(string movementGuid)
+    :MovimientoException($"Not revocable movement with Id {movementGuid}");
